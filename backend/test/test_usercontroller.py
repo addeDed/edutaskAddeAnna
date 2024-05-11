8e29bfdd1eaf227a89cbb69e577281488eb42555
blixtenA <anbx22@bth.se>
2024-05-10 22:12:02 -0400

    general pylint cleanup

--- backend/test/test_usercontroller.py
+++ backend/test/test_usercontroller.py
@@ -11,18 +11,18 @@ class TestUserController:
     '''
     TestUserController class docstring
     '''
-# TEST 1
+    # TEST 1
     @pytest.mark.parametrize("test_input,expected",
-                             [('invalidemail.com',False),
-                              ('invalid.email.com',False),
-                              ('invalidemail@',False),
-                              ('@invalidemail.com',False),
-                              ('invalidemail@.com',False),
-                              ('invalidemail@domain',False),
-                              ('invalid email@example.com',False),
-                              ('invalid!email@example.com',False),
-                              ('invalidemail',False),
-                              ('valid@email.com',True)
+                             [('invalidemail.com', False),
+                              ('invalid.email.com', False),
+                              ('invalidemail@', False),
+                              ('@invalidemail.com', False),
+                              ('invalidemail@.com', False),
+                              ('invalidemail@domain', False),
+                              ('invalid email@example.com', False),
+                              ('invalid!email@example.com', False),
+                              ('invalidemail', False),
+                              ('valid@email.com', True)
                              ]
                             )
     def test_invalid_email_format(self, mocker, test_input, expected):
@@ -38,27 +38,30 @@ class TestUserController:
         try:
             controller.get_user_by_email(test_input)
             print("No error raised for ", test_input)
-            assert True == expected
-        except ValueError as exept:
+            assert expected == True
+        except ValueError:
             print("ValueError raised for ", test_input)
-            assert False == expected
-        
+            assert expected == False
+
         #dao_mock.find.return_value = [{'email': 'test@example.com'}]
 
-# TEST 2a
+    # TEST 2a
     def test_query_database_for_user(self, mocker):
         """
-        Test case to verify that the UserController queries the database for a user with the provided email address.
+        Test case to verify that the UserController queries the database for a user with
+        the provided email address.
 
         Purpose:
-        This test verifies that when the `get_user_by_email` method of the UserController is called with a valid email address,
-        it attempts to query the database for a user with that email address.
+        This test verifies that when the `get_user_by_email` method of the UserController
+        is called with a valid email address, it attempts to query the database for a
+        user with that email address.
 
         Steps:
         1. Mock the DAO object.
         2. Create a UserController instance with the mocked DAO.
         3. Mock the `find` method of the DAO object to raise an error when called.
-        4. Call the `get_user_by_email` method of the UserController with a valid email address.
+        4. Call the `get_user_by_email` method of the UserController with a valid email
+           address.
         5. Assert that the `find` method of the DAO object was called once.
 
         """
@@ -71,26 +74,29 @@ class TestUserController:
         except Exception:
             pass  # Ignore any exceptions raised
 
-        print("Mocked DAO calls:")
-        print(dao_mock.method_calls)
+        #print("Mocked DAO calls:")
+        #print(dao_mock.method_calls)
 
         # Assert that the DAO's find method was called once
         dao_mock.find.assert_called_once()
 
-# TEST 2b
+    # TEST 2b
     def test_query_database_for_user_none(self, mocker):
         """
-        Test case to verify that the UserController does not query the database when an empty email address is provided.
+        Test case to verify that the UserController does not query the database when an
+        empty email address is provided.
 
         Purpose:
-        This test verifies that when the `get_user_by_email` method of the UserController is called with an empty email address,
+        This test verifies that when the `get_user_by_email` method of the UserController
+        is called with an empty email address,
         it does not attempt to query the database.
 
         Steps:
         1. Mock the DAO object.
         2. Create a UserController instance with the mocked DAO.
         3. Mock the `find` method of the DAO object to raise an error when called.
-        4. Call the `get_user_by_email` method of the UserController with an empty email address.
+        4. Call the `get_user_by_email` method of the UserController with an empty email
+           address.
         5. Assert that the `find` method of the DAO object was not called.
 
         """
@@ -103,16 +109,17 @@ class TestUserController:
         except Exception:
             pass  # Ignore any exceptions raised
 
-        print("Mocked DAO calls:")
-        print(dao_mock.method_calls)
+        #print("Mocked DAO calls:")
+        #print(dao_mock.method_calls)
 
         # Assert that the DAO's find method was not called
         dao_mock.find.assert_not_called()
 
-# TEST 3a
+    # TEST 3a
     def test_user_with_unique_entry(self, mocker):
         """
-        Test the behavior of the get_user_by_email method when provided with an email address that has a unique entry in the database.
+        Test the behavior of the get_user_by_email method when provided with an email
+        address that has a unique entry in the database.
         It should return the user object associated with that email address.
         """
         dao_mock = mocker.MagicMock()
@@ -125,11 +132,13 @@ class TestUserController:
         # Assert that the result matches the expected result
         assert result == {'email': 'user1@example.com'}
 
-# TEST 3b
+    # TEST 3b
     def test_user_with_non_unique_entry(self, mocker, capsys):
         """
-        Test the behavior of the get_user_by_email method when provided with an email address that has multiple entries in the database.
-        It should raise an exception and return the first user object associated with that email address.
+        Test the behavior of the get_user_by_email method when provided with an email
+        address that has multiple entries in the database.
+        It should raise an exception and return the first user object associated with
+        that email address.
         """
         dao_mock = mocker.MagicMock()
         dao_mock.find.return_value = [{'email': 'user2@example.com'},
@@ -143,16 +152,14 @@ class TestUserController:
         captured = capsys.readouterr()
         assert "Error: more than one user found with mail" in captured.out
 
-        # Assert that an exception is raised
-        #assert exc_info.type == Exception
-
         # Assert that the result matches the expected result (first user entry)
         assert result == {'email': 'user2@example.com'}
 
-# TEST 3c
+    # TEST 3c
     def test_user_without_entry(self, mocker):
         """
-        Test the behavior of the get_user_by_email method when provided with an email address that does not have any entry in the database.
+        Test the behavior of the get_user_by_email method when provided with an email
+        address that does not have any entry in the database.
         updated - It should return None (not implemented in function)
         """
         dao_mock = mocker.MagicMock()
@@ -161,11 +168,11 @@ class TestUserController:
         try:
             result = controller.get_user_by_email('user3@example.com')
         except Exception as exept:
-            print("Unexpected exception type:",type(exept).__name__)
+            print("Unexpected exception type:", type(exept).__name__)
             result = 'AssertionError'
-        assert result == None
+        assert result is None
 
-# TEST 4
+    # TEST 4
     def test_get_user_by_email_database_failure(self, mocker):
         """
         Test the behavior of the get_user_by_email method when a database operation fails.
